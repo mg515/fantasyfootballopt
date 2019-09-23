c6c9f602c75c465c78b593a7fd3a2d3b51f673c8
Miha <miha.garafolj@gmail.com>
2019-09-23 23:07:10 +0200

    refactor, played with bps metric

---
+++ fantasy_lp.R
@@ -0,0 +1,32 @@
+require(Rglpk)
+require(dplyr)
+
+
+fantasy_lp <- function(player_table, f.obj, budget, team_size = c(2,5,5,3)){
+
+    # add the constraints based on game rules
+    f.con <- rbind(
+                rep(1, n),                                  # team in size of 15
+                player_table$now_cost,                      # budget constraint
+                as.numeric(player_table$element_type == 1), # goalkeepers
+                as.numeric(player_table$element_type == 2), # defense
+                as.numeric(player_table$element_type == 3), # midfield
+                as.numeric(player_table$element_type == 4)  # attack
+                )
+    f.dir <- c('<=', '<=', '==', '==', '==', '==')
+    f.rhs <- c(15, budget * 10, team_size)
+
+    # add max-3-per-team constraints
+    for (team in 1:20){
+
+        teamBool = as.numeric(player_table$team == team)
+
+        f.con <- rbind(f.con, teamBool)
+        f.dir <- c(f.dir, '<=')
+        f.rhs <- c(f.rhs, 3)
+    }
+
+    # run the LP
+    res <- Rglpk_solve_LP(f.obj, f.con, f.dir, f.rhs, max=TRUE, types="B")
+    return(res)
+}
--- lp_script.R
+++ lp_script.R
@@ -1,45 +1,31 @@
 require(dplyr)
 require(magrittr)
 require(tibble)
-require(Rglpk)
+source("fantasy_lp.R")
 
 
-players <- read.csv('Fantasy-Premier-League/data/2019-20/players_raw.csv') %>%
-            select(first_name, second_name, team, element_type, now_cost,ict_index) %>%
-            as.tbl
+players <- read.csv('Fantasy-Premier-League/data/2019-20/players_raw.csv', stringsAsFactors=FALSE) %>% as.tbl
 
 n <- nrow(players)
 budget <- 100
 
 # optimization based on ict_index
-f.obj <- players %>% pull(ict_index)
+ict <- players$ict_index
+bps <- players$bps
 
-# add the constraints based on game rules
-f.con <- rbind(
-            rep(1, n),                             # team in size of 15
-            players$now_cost,                      # budget constraint
-            as.numeric(players$element_type == 1), # goalkeepers
-            as.numeric(players$element_type == 2), # defense
-            as.numeric(players$element_type == 3), # midfield
-            as.numeric(players$element_type == 4)  # attack
-            )
-f.dir <- c('<=', '<=', '==', '==', '==', '==')
-f.rhs <- c(15, budget * 10, 2, 5, 5, 3)
+probs <- players$chance_of_playing_next_round
+probs[probs=='None'] <- 0
+probs <- as.numeric(probs) / 100
 
-# add max-3-per-team constraints
-for (team in unique(players$team)){
 
-    teamBool = as.numeric(players$team == team)
+# objective function
+f.obj <- ict * probs
 
-    f.con <- rbind(f.con, teamBool)
-    f.dir <- c(f.dir, '<=')
-    f.rhs <- c(f.rhs, 3)
-}
-
-# run the LP
-res <- Rglpk_solve_LP(f.obj, f.con, f.dir, f.rhs, max=TRUE, types="B")
+# run lp
+res <- fantasy_lp(players, f.obj, budget=100)
 
 # check the optimal team
 players %<>% mutate(solution=res$solution)
 players %>% filter(solution==1) %>%
-            arrange(-ict_index)
+            select(first_name, second_name, team, element_type, now_cost, chance_of_playing_next_round, ict_index, bps) %>%
+            arrange(element_type)

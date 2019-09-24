fb1cf64e58e67850cd84fdf00bae516bd073b7ce
Miha <miha.garafolj@gmail.com>
2019-09-24 14:00:23 +0200

    added gameweek selection

--- functions.R
+++ functions.R
@@ -8,10 +8,11 @@ fantasy_lp <- function(
     budget=100,             # budget of the team
     team_size = c(2,5,5,3)  # team size constraints (gk, def, mid, att)
 ){
+    n_players <- nrow(player_table)
 
     # add the constraints based on game rules
     f.con <- rbind(
-                rep(1, n),                                  # team in size of 15
+                rep(1, n_players),                                  # team in size of 15
                 player_table$now_cost,                      # budget constraint
                 as.numeric(player_table$element_type == 1), # goalkeepers
                 as.numeric(player_table$element_type == 2), # defense
@@ -23,13 +24,34 @@ fantasy_lp <- function(
 
     # add max-3-per-team constraints
     for (team in 1:20){
-
         teamBool = as.numeric(player_table$team == team)
-
+        
         f.con <- rbind(f.con, teamBool)
         f.dir <- c(f.dir, '<=')
         f.rhs <- c(f.rhs, 3)
     }
+
+    # run the LP
+    res <- Rglpk_solve_LP(f.obj, f.con, f.dir, f.rhs, max=TRUE, types="B")
+    return(res)
+}
+
+
+weekteam_lp <- function(
+    team_table,
+    f.obj,
+    team_size=c(1,3,2,1)
+){
+    # 1 goalkeeper, atleast 3 def, 2 mid and 1 att
+    f.con <- rbind(
+                rep(1, 15),                               # team size always 15
+                as.numeric(team_table$element_type == 1), # goalkeepers
+                as.numeric(team_table$element_type == 2), # defense
+                as.numeric(team_table$element_type == 3), # midfield
+                as.numeric(team_table$element_type == 4)  # attack
+                )
+    f.dir <- c('<=', '==', '>=', '>=', '>=')
+    f.rhs <- c(11, team_size)
 
     # run the LP
     res <- Rglpk_solve_LP(f.obj, f.con, f.dir, f.rhs, max=TRUE, types="B")
--- lp_script.R
+++ lp_script.R
@@ -5,8 +5,6 @@ source("functions.R")
 
 
 players <- read.csv('Fantasy-Premier-League/data/2019-20/players_raw.csv', stringsAsFactors=FALSE) %>% as.tbl
-
-n <- nrow(players)
 budget <- 100
 
 # optimization based on ict_index
@@ -19,13 +17,23 @@ probs <- as.numeric(probs) / 100
 
 
 # objective function
-f.obj <- ict * probs
+f.obj <- ict
 
 # run lp
 res <- fantasy_lp(players, f.obj, budget=100)
 
-# check the optimal team
-players %<>% mutate(solution=res$solution)
-players %>% filter(solution==1) %>%
-            select(first_name, second_name, team, element_type, now_cost, chance_of_playing_next_round, ict_index, bps) %>%
-            arrange(element_type)
+# select the optimal overall team
+players_opt <- players %>%
+                mutate(solution=res$solution) %>%
+                filter(solution==1) %>%
+                select(first_name, second_name, team, element_type, now_cost, chance_of_playing_next_round, ict_index, bps) %>%
+                arrange(-ict_index)
+players_opt
+
+# select the optimal gameweek team based on price
+team_res <- weekteam_lp(players_opt, players_opt$now_cost)
+team_opt <- players_opt %>%
+                mutate(team_solution=team_res$solution) %>%
+                select(-bps,-chance_of_playing_next_round) %>%
+                arrange(element_type)
+team_opt

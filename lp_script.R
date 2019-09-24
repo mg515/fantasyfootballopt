be362daf709ca211c38a4171cfeebab2a775f712
Miha <miha.garafolj@gmail.com>
2019-09-24 16:02:42 +0200

    trying out new metrics

--- lp_script.R
+++ lp_script.R
@@ -7,17 +7,19 @@ source("functions.R")
 players <- read.csv('Fantasy-Premier-League/data/2019-20/players_raw.csv', stringsAsFactors=FALSE) %>% as.tbl
 budget <- 100
 
-# optimization based on ict_index
+# objective function candidates
 ict <- players$ict_index
 bps <- players$bps
+selected_by <- players$selectted_by_percent
+transfers_in <- players$transfers_in_event
 
 probs <- players$chance_of_playing_next_round
 probs[probs=='None'] <- 0
 probs <- as.numeric(probs) / 100
 
 
-# objective function
-f.obj <- ict
+# select the objective function
+f.obj <- transfers_in
 
 # run lp
 res <- fantasy_lp(players, f.obj, budget=100)
@@ -26,14 +28,12 @@ res <- fantasy_lp(players, f.obj, budget=100)
 players_opt <- players %>%
                 mutate(solution=res$solution) %>%
                 filter(solution==1) %>%
-                select(first_name, second_name, team, element_type, now_cost, chance_of_playing_next_round, ict_index, bps) %>%
                 arrange(-ict_index)
-players_opt
+players_opt %>% select(first_name, second_name, team, element_type, now_cost, chance_of_playing_next_round, selected_by_percent,ict_index, bps)
 
 # select the optimal gameweek team based on price
 team_res <- weekteam_lp(players_opt, players_opt$now_cost)
 team_opt <- players_opt %>%
-                mutate(team_solution=team_res$solution) %>%
-                select(-bps,-chance_of_playing_next_round) %>%
+                mutate(team_solution=team_res$solution)
                 arrange(element_type)
-team_opt
+team_opt %>% select(first_name, second_name, team, element_type, now_cost, transfers_in_event)
